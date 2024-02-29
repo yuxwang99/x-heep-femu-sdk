fecf6bf88e34261efe0d4d4002326022093eaa18
yuxwang99 <yuxwang.1999@hotmail.com>
2024-02-29 09:37:47 +0000

    include csr for fp32

--- sw/riscv/apps/floating_point_test/floating_point_test.c
+++ sw/riscv/apps/floating_point_test/floating_point_test.c
@@ -8,15 +8,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "csr.h"
 
 int main(int argc, char *argv[])
 {
-   float a = 0.5;
-   float b = 1.3;
-   printf("int a: %d\n", (int)a);
-   printf("%.4f + %.4f = %.4f\n", a, b, a+b);
-   printf("%f - %f = %f\n", a, b, a-b);
-   printf("%f x %f = %f\n", a, b, a*b);
-   printf("%f / %f = %f\n", a, b, a/b);
-   return EXIT_SUCCESS;
+    CSR_SET_BITS(CSR_REG_MSTATUS, (0x1 << 13));
+    float a = 0.5;
+    float b = 1.3;
+    printf("int a: %d\n", (int)a);
+    printf("%.4f + %.4f = %.4f\n", a, b, a+b);
+    printf("%f - %f = %f\n", a, b, a-b);
+    printf("%f x %f = %f\n", a, b, a*b);
+    printf("%f / %f = %f\n", a, b, a/b);
+    return EXIT_SUCCESS;
 }
