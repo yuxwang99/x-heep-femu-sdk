cd6040cdbf4e23256d291aec8af92028e29b556e
yuxwang99 <yuxwang.1999@hotmail.com>
2024-03-05 08:31:02 +0000

    fix putfloat

--- sw/riscv/apps/floating_point_test/floating_point_test.c
+++ sw/riscv/apps/floating_point_test/floating_point_test.c
@@ -12,146 +12,50 @@
 #include "csr.h"
 
 
+typedef enum
+{
+    DEC1 = 10,
+    DEC2 = 100,
+    DEC3 = 1000,
+    DEC4 = 10000,
+    DEC5 = 100000,
+    DEC6 = 1000000,
+
+} tPrecision ;
+
 void swap(char *a, char *b) {
     char tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-// A function to print a floating point number using putchar
-void putfloat(float x, int p)
+void putLong(long x)
 {
-    // Check if x is negative
-    if (x < 0)
+    if(x < 0)
     {
-        // Print a minus sign
         putchar('-');
-        // Make x positive
         x = -x;
     }
-
-    // Convert the integer part of x into a string of digits
-    long i = (long)x; // Get the integer part
-    char int_str[20]; // An array to store the digits
-    int len = 0; // The length of the string
-    int loop_times = 0;
-    do
+    if (x >= 10) 
     {
-        // Get the last digit and store it in the array
-        int_str[len] = '0' + i % 10;
-        len++;
-        // Remove the last digit from i
-        i /= 10;
-        loop_times++;
-    } while (i > 0 && loop_times<5);
-
-    // Reverse the string of digits
-    for (int j = 0; j < len / 2; j++)
-    {
-        // Swap the elements at both ends
-        swap(&int_str[j], &int_str[len - 1 - j]);
+        putLong(x / 10);
     }
-
-    // Print the string of digits
-    for (int j = 0; j < len; j++)
-    {
-        putchar(int_str[j]);
-    }
-
-    // Print a decimal point
-    putchar('.');
-
-    // Convert the fractional part of x into a string of digits
-    float f = x - (long)x; // Get the fractional part
-    char frac_str[20]; // An array to store the digits
-    len = 0; // The length of the string
-    
-    while (p--)
-    {
-        // Get the first digit after the decimal point and store it in the array
-        f = (f - (long)f) * 10;
-        frac_str[len] = '0' + (long)f;
-        len++;
-        
-
-    }
-    // Round up if necessary
-    if (fabs(f - (long)f) >= 0.5f)
-    {
-        frac_str[len - 1]++;
-    }
-
-    // Print the string of digits
-    for (int j = 0; j < len; j++)
-    {
-        putchar(frac_str[j]);
-    }
+    putchar(x % 10+'0');
 }
 
-void putfloat_wang(float x, int p)
+void putFloat( float f, tPrecision p )
 {
-    // Check if x is negative
-    if (x < 0)
+    long i = (long)f ;
+    putLong( i ) ;
+    f = (f - i) * p ;
+    i = abs((long)f) ;
+    if( fabs(f) - i >= 0.5f )
     {
-        // Print a minus sign
-        putchar('-');
-        // Make x positive
-        x = -x;
+        i++ ;
     }
-
-    // Convert the integer part of x into a string of digits
-    long i = (long)x; // Get the integer part
-    char int_str[20]; // An array to store the digits
-    int int_len = 0; // The length of the string
-    do
-    {
-        // Get the last digit and store it in the array
-        int_str[int_len] = '0' + i % 10;
-        int_len++;
-        // Remove the last digit from i
-        i /= 10;
-    } while (i > 0);
-
-    
-    // Convert the fractional part of x into a string of digits
-    float f = x - (long)x; // Get the fractional part
-    char frac_str[20]; // An array to store the digits
-    int len = 0; // The length of the string
-    
-    while (p--)
-    {
-        // Get the first digit after the decimal point and store it in the array
-        f = (f - (long)f) * 10;
-        frac_str[len] = '0' + (long)f;
-        len++;
-        // Round up if necessary
-        if (p==0 && fabs(f - (long)f) >= 0.5f)
-        {   
-            // printf("round %d\n",p);
-            frac_str[len - 1]++;
-        }
-    }
-
-    // Print the string of digits
-    printf("digit len: %d\n", len);
-    // Print the string of digits
-    for (int j = 0; j < int_len; j++)
-    {
-        putchar(int_str[j]);
-    }
-
-    // Print a decimal point
-    putchar('.');
-
-
-    for (int j = 0; j < len; j++)
-    {
-        putchar(frac_str[j]);
-    }
-
-
+    putchar('.') ;
+    putLong( i ) ;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -165,19 +69,89 @@ int main(int argc, char *argv[])
     float divAB = a / b;
     printf("debug float\n");
     
-    // putfloat(a, 1);
-    
-    // putfloat(b, 1);
-    // printf(" = ");
-
-    putfloat(divAB, 2);
-
-    // putfloat(a, 4);
-    // printf(" - ");
-    // putfloat(b, 4);
-    // printf(" = ");
-    // putfloat(a-b, 4);
-    // printf("\n");
+    putFloat(a, DEC4);
+    printf(" + ");
+    putFloat(b, DEC4);
+    printf(" = ");
+    putFloat(sumAB, DEC4);
+    putchar('\n');
+    putFloat(prodAB, DEC4);
+    putchar('\n');
+    putFloat(diffAB, DEC5);
+    putchar('\n');
+    putFloat(divAB, DEC3);
+    putchar('\n');
 
     return EXIT_SUCCESS;
 }
+
+
+// // A function to print a floating point number using putchar
+// void putfloat(float x, int p)
+// {
+//     // Check if x is negative
+//     if (x < 0)
+//     {
+//         // Print a minus sign
+//         putchar('-');
+//         // Make x positive
+//         x = -x;
+//     }
+
+//     // Convert the integer part of x into a string of digits
+//     long i = (long)x; // Get the integer part
+//     char int_str[20]; // An array to store the digits
+//     int len = 0; // The length of the string
+//     int loop_times = 0;
+//     do
+//     {
+//         // Get the last digit and store it in the array
+//         int_str[len] = '0' + i % 10;
+//         len++;
+//         // Remove the last digit from i
+//         i /= 10;
+//         loop_times++;
+//     } while (i > 0 && loop_times<5);
+
+//     // Reverse the string of digits
+//     for (int j = 0; j < len / 2; j++)
+//     {
+//         // Swap the elements at both ends
+//         swap(&int_str[j], &int_str[len - 1 - j]);
+//     }
+
+//     // Print the string of digits
+//     for (int j = 0; j < len; j++)
+//     {
+//         putchar(int_str[j]);
+//     }
+
+//     // Print a decimal point
+//     putchar('.');
+
+//     // Convert the fractional part of x into a string of digits
+//     float f = x - (long)x; // Get the fractional part
+//     char frac_str[20]; // An array to store the digits
+//     len = 0; // The length of the string
+    
+//     while (p--)
+//     {
+//         // Get the first digit after the decimal point and store it in the array
+//         f = (f - (long)f) * 10;
+//         frac_str[len] = '0' + (long)f;
+//         len++;
+        
+
+//     }
+//     // Round up if necessary
+//     if (fabs(f - (long)f) >= 0.5f)
+//     {
+//         frac_str[len - 1]++;
+//     }
+
+//     // Print the string of digits
+//     for (int j = 0; j < len; j++)
+//     {
+//         putchar(frac_str[j]);
+//     }
+// }
